913581952ffe047419ac3286fb17ae10bdb4ddd8
dshaw <diego.belchior@gmail.com>
2019-04-25 18:33:27 -0300

    ai vai

--- Fleury/analise_classificacao_atestados/libs/ana_class_atest_persist.py
+++ Fleury/analise_classificacao_atestados/libs/ana_class_atest_persist.py
@@ -1,20 +1,39 @@
 # -*- coding: utf-8 -*-
 
+import sys
 import sqlite3
 
 import libs.ana_class_atest_log as log
+
 
 import configparser
 config = configparser.RawConfigParser(allow_no_value=True)
 config.read('./conf/ana_class_atest.cfg')
 
-# definir nome do db.
+database = config.get('sqlite','database')
+table    = config.get('sqlite','table')
 
-# se arquivo nao existir, criar.
+def write_to_sqlite():
 
-# se tabela nao existir, criar.
+    stmt_create = """
+        CREATE TABLE teste (
+            id integer PRIMARY KEY,
+            name text NOT NULL,
+            begin_date text,
+            end_date text)
+        """
 
-# comando insert.
+    # stmt_insert =""" """
 
-# inserir passo a passo da execucao.
-
+    try:
+        con = sqlite3.connect(database)
+        cur = con.cursor()
+        cur.execute(stmt_create)
+        cur.commit()
+    
+        cur.execute()
+    
+        con.close()
+    except Exception as e:
+        log.logger.exception('Falha ao gravar no SQLite: ' + str(e))
+ 
---
+++ Fleury/analise_classificacao_atestados/teste.py
@@ -0,0 +1,4 @@
+from libs.ana_class_atest_persist import write_to_sqlite
+
+write_to_sqlite()
+
