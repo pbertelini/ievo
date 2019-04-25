22248efed9a5a58aa9dd51de15f51954679e0c95
dshaw <diego.belchior@gmail.com>
2019-04-25 15:02:24 -0300

    ai vai

--- Fleury/analise_classificacao_atestados/libs/ana_class_atest_persist.py
+++ Fleury/analise_classificacao_atestados/libs/ana_class_atest_persist.py
@@ -2,3 +2,19 @@
 
 import sqlite3
 
+import libs.ana_class_atest_log as log
+
+import configparser
+config = configparser.RawConfigParser(allow_no_value=True)
+config.read('./conf/ana_class_atest.cfg')
+
+# definir nome do db.
+
+# se arquivo nao existir, criar.
+
+# se tabela nao existir, criar.
+
+# comando insert.
+
+# inserir passo a passo da execucao.
+
