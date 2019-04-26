bfbce5c95008a29667ce106b7f0e5c18dd1636bd
dshaw <diego.belchior@gmail.com>
2019-04-26 00:53:06 -0300

    email novo ok!

--- Fleury/analise_classificacao_atestados/libs/ana_class_atest_notification.py
+++ Fleury/analise_classificacao_atestados/libs/ana_class_atest_notification.py
@@ -19,16 +19,27 @@ def email(subj, text, dest):
         e_user = config.get('email','user')
         e_pass = config.get('email','pass')
         e_smtp = config.get('email','smtp')
-        e_logo = config.get('email','logo')
-    
+        
+        cabecalho = config.get('email','cabecalho')
+        rodape    = config.get('email','rodape')
+        
         msg = MIMEMultipart()
         msg['Subject'] = subj
         msg['From'] = e_from
         msg['To'] = e_user
-        
-        with open(e_logo, 'rb') as f:
-            mime = MIMEBase('image', 'png', filename='e_logo.png')
-            mime.add_header('Content-Disposition', 'attachment', filename=e_logo)
+
+        with open(cabecalho, 'rb') as f:
+            mime = MIMEBase('image', 'png', filename='cabecalho.png')
+            mime.add_header('Content-Disposition', 'attachment', filename=cabecalho)
+            mime.add_header('X-Attachment-Id', '1')
+            mime.add_header('Content-ID', '<1>')
+            mime.set_payload(f.read())
+            encoders.encode_base64(mime)
+            msg.attach(mime)
+
+        with open(rodape, 'rb') as f:
+            mime = MIMEBase('image', 'png', filename='rodape.png')
+            mime.add_header('Content-Disposition', 'attachment', filename=rodape)
             mime.add_header('X-Attachment-Id', '0')
             mime.add_header('Content-ID', '<0>')
             mime.set_payload(f.read())
